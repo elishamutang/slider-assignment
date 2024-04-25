2dfa9be736f2f46ceb5edf071f11fa1ca1a9b2bd
Elisha <138951172+elishamutang@users.noreply.github.com>
2024-04-25 14:27:31 +0800

    Initial commit

---
+++ webpack.prod.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = merge(common ,{
+    mode: 'production',
+    plugins: [new MiniCSSExtractPlugin({filename: "styles.css"})],
+    module: {
+        rules: [
+        {
+            test: /\.css$/i,
+            use: [MiniCSSExtractPlugin.loader, "css-loader"],
+        },
+        ]
+    },
+    optimization: {
+        minimizer: [
+        '...',
+        new CssMinimizerWebpackPlugin(),
+        ]
+    }
+});
