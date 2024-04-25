76f8697c2ce18016d1b557119a5d854774c7dc3a
elishamutang <elishamutang@outlook.com>
2024-04-25 19:02:30 +0800

    initial stage.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,73 @@
 import './styles.css';
+import pictures from './images';
 
+const sliderContainer = document.querySelector('.slider-container');
+const sliderFrame = document.querySelector('.slider-frame');
+
+// Append first img.
+const initialImg = document.createElement('img');
+initialImg.src = Object.keys(pictures).map((key, idx) => {
+
+    if(idx === 0) {
+        initialImg.id = key;
+    }
+
+    return pictures[key];
+
+}).filter((url, idx) => {
+
+    if(idx === 0) {
+        return url;
+    }
+    
+});
+
+
+sliderFrame.append(initialImg);
+
+let currentImg = initialImg.id;
+
+// Navigate between images
+sliderContainer.addEventListener('click', (e) => {
+    
+    if(e.target.tagName === 'BUTTON') {
+
+        if(e.target.textContent === 'Right') {
+
+            let pictureIdx = Object.keys(pictures).map((key, idx) => {
+
+                if(key === currentImg) {
+                    return idx;
+                }
+
+            }).filter((index) => {
+                return index !== undefined;
+            });
+
+            pictureIdx++
+
+            initialImg.src = Object.keys(pictures).map((key, idx) => {
+
+                if(idx === pictureIdx) {
+                    currentImg = key;
+                    initialImg.id = key;
+                    return pictures[key];
+                }
+
+            }).filter((url) => {
+                return url !== undefined;
+            })
+
+            console.log(pictureIdx);
+
+        } else {
+
+
+
+
+        }
+
+        
+    }
+
+})
